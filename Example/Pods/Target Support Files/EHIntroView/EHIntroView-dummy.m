bb4270fbb3b57d34202104833fd2c87392598552
Eric Huang <WaterFlowsEast@gmail.com>
2017-01-11 09:02:19 +0800

    update podspec

---
+++ Example/Pods/Target Support Files/EHIntroView/EHIntroView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_EHIntroView : NSObject
+@end
+@implementation PodsDummy_EHIntroView
+@end
---
+++ Example/Pods/Target Support Files/EHIntroView/EHIntroView-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double EHIntroViewVersionNumber;
+FOUNDATION_EXPORT const unsigned char EHIntroViewVersionString[];
+
